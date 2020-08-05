24dfd5cea9953bad354c5cdfcd8654d9d1aab3f2
Amr Ezz <amrezz0111@gmail.com>
2020-08-05 20:30:02 +0200

    Finished 8-ball-app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[500],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  Ball({Key key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int number = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        child: Center(
+          child: FlatButton(
+            onPressed: () {
+              setState(() {
+                number = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$number.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
